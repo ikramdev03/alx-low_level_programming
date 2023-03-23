02be27ef8c08eb87d3be3e473d04b5fae98046a3
ikram <izmerikram03#gmail.com>
2023-03-22 20:16:33 -0700

    ikraam

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,33 +7,39 @@
  */
 void print_times_table(int n)
 {
-int i, j, k;
-if (n >= 0 && n <= 15)
-{
-for (i = 0; i <= n; i++)
-{
-_putchar('0');
-for (j = 1; j <= n; j++)
-{
-_putchar(',');
-_putchar(' ');
-k = j * i;
-if (k == 99)
-_putchar(' ');
-if (k == 9)
-_putchar(' ');
-if (k >= 100)
-{
-_putchar((k / 100) + '0');
-_putchar(((k / 10)) % 10 + '0');
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
 }
-else if (k <= 99 && k >= 10)
-{
-_putchar((k / 10) + '0');
-}
-_putchar((k % 10) + '0');
-}
-_putchar('\n');
-}
-}
-}
+
