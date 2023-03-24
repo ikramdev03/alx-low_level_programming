3e9f6380981e156a36ebb2619dad6a6075bdbe32
ikram <izmerikram03#gmail.com>
2023-03-24 05:51:00 -0700

    ikraam

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,21 +5,24 @@
  * Bwave ICT file
  * Return: no return
  */
+
 void more_numbers(void)
 {
-int num, row, count;
-for (row = 1; row <= 10; row++)
-{
-for (count = 0; count <= 14; count++)
-{
-num = count;
-if (count > 9)
-{
-_putchar(1 + 48);
-num = count % 10;
-}
-_putchar(num + 48);
-}
-_putchar("\n");
-}
+	int num, row, count;
+
+	for (row = 1; row <= 10; row++)
+	{
+		for (count = 0; count <= 14; count++)
+
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar("\n");
+	}
 }
