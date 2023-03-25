cfa5034b901ccaa887a7b6d80498a1d5cebc4c9f
ikram <izmerikram03#gmail.com>
2023-03-24 18:49:17 -0700

    ikkram

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,24 +1,36 @@
 #include "main.h"
-
 /**
- * print_number - prints an integer
- * @n:.input integer parameter
+ * print_number -  function that prints an integer.
  *
- *Owned by Bwave ICT/ Bright Daniel
- */
+ * @n: is the integer
+*/
 
 void print_number(int n)
 {
-unsigned int num = n;
-/*first check if its negative*/
-if (n < 0)
-{
-_putchar('-');
-num = -num;
-}
-/*print the first few digits*/
-if (num / 10)
-print_number(num / 10);
-/*print the last digit*/
-_putchar((num % 10 + '0');
+	int last, main,  rev = 0;
+	char flag = 'n';
+
+	main = n;
+	while (main)
+	{
+		last = main % 10;
+		rev *= 10;
+		rev += last;
+		main = main / 10;
+	}
+	if (rev < 0)
+	{
+		flag = 'y';
+		rev *= -1;
+	}
+	if (flag == 'y')
+	{
+		_putchar('-');
+	}
+	while (rev)
+	{
+		last = rev % 10;
+		_putchar(last + '0');
+		rev = rev / 10;
+	}
 }
