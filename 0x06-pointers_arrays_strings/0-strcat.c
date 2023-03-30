c0504396a1248e587293eac87c9ecee54f1c434b
ikramdev03 <izmerikram03@gmail.com>
2023-03-30 14:00:46 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,20 +6,22 @@
  * @src: copy from
  * Return: pointer to dest
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
+		/*do nothing, just find the end of dest*/
+	}
+		
 	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
-		j++;
 	}
-	}
+	
 	dest[i] = '\0';
 	return (dest);
 }
