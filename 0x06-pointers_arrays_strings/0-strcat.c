889ce34a98b05fe638c59072af530464a85e29e2
ikramdev03 <izmerikram03@gmail.com>
2023-03-30 11:19:19 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,21 +2,22 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest: char copy to
- * @src: char copy from
- * Return: char pointer to dest
+ * @dest: copy to
+ * @src: copy from
+ * Return: pointer to dest
  */
 char *_strcat(char *dest, char *src);
 {
 	int i;
 	int j;
-for (i = 0; dest[i] != '\0'; i++)
-for (j = 0; src[i] != '\0'; j++)
-{
-dest[i] = src[i];
-i++;
-j++;
-}
-dest[i] = '\0';
-return (dest);
+
+	for (i = 0; dest[i] != '\0'; i++)
+	for (j = 0; src[i] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
