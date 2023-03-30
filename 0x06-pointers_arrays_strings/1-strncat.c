37cfebd5ac93c01d912f9077dacc1cbab028a731
ikramdev03 <izmerikram03@gmail.com>
2023-03-30 14:51:57 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	/* Find the end of dest string */
+	}
+	/* Concatenate src to dest until n characters or end of src is reached */
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	/* Terminate the resulting string with a null character */
+	dest[i] = '\0';
+	return (dest);
+}
