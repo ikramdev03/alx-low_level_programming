abfe58130f897c7b577432a469e8d2eaa39dbda6
ikramdev03 <izmerikram03@gmail.com>
2023-04-06 08:11:09 -0700

    6. Inception. Is it possible?

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,6 +14,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int acual_prime(int n, int i);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
