3a313501154071a0769799c1d735054b98ad64a8
ikramdev03 <izmerikram03@gmail.com>
2023-04-06 16:55:35 -0700

    sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: numbre to calculate the square root of
+ * @n: number to calculate the square root of
  *
  * Return: resulting the square root n
  * if n does not have a natural sqaart root, the function should return -1
