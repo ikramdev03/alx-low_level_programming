c6a3b223735c57801278040e9aa4fe9137772846
ikramdev03 <izmerikram03@gmail.com>
2023-04-06 11:21:30 -0700

    ikra

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,8 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 
 int (check_pal(char *s, int i, int len));
 int _strlen_recursion(char *s);
+
 
 /**
  * is_palindrome - checks if a string is a palindrome
@@ -14,11 +14,10 @@ int _strlen_recursion(char *s);
 int is_palindrome(char *s)
 {
 	if (*s == 0)
-	{
 		return (1);
-	}
 	return (check_pal(s, 0, _strlen_recursion(s)));
 }
+
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string to calculate the length of
@@ -28,9 +27,7 @@ int is_palindrome(char *s)
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		return (0);
-	}
 	return (1 + _strlen_recursion(s + 1));
 }
 
@@ -46,12 +43,8 @@ int _strlen_recursion(char *s)
 int check_pal(char *s, int i, int len)
 {
 	if ((*s + 1) != *(s + len + 1))
-	{
 		return (0);
-	}
 	if (len >= 0)
-	{
 		return (1);
-	}
 	return (check_pal(s, i + 1, len - 1));
 }
