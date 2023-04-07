8868f5c4bf9bbc2619d34f7f40f9cf8e22a4aa98
ikramdev03 <izmerikram03@gmail.com>
2023-04-06 18:46:51 -0700

    s_prime_number:

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,20 @@
 #include "main.h"
 
+int acual_prime(int n, int i);
+
+/**
+ * is_prime_number - says if integer is a prime number or not
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is a prime number, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return (0);
+	return (acual_prime(n, n - 1));
+}
+
 /**
  * acual_prime - calculates if a number prime recursively
  *
@@ -19,19 +34,4 @@ int acual_prime(int n, int i)
 		return (0);
 	}
 	return (acual_prime(n, i - 1));
-}
-
-/**
- * is_prime_number - says if integer is a prime number or not
- * @n: number to evaluate
- *
- * Return: 1 if n is a prime number, 0 if not
- */
-int is_prime_number(int n)
-{
-	if (n <= 0)
-	{
-		return (0);
-	}
-	return (acual_prime(n, n - 1));
 }
