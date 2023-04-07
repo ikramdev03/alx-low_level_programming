a095253e1598ebca8c4c9fe7327139ecf1dd1cd1
ikramdev03 <izmerikram03@gmail.com>
2023-04-06 18:48:54 -0700

    s_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -25,13 +25,9 @@ int is_prime_number(int n)
  */
 int acual_prime(int n, int i)
 {
-	if (i == 0)
-	{
+	if (i == 1)
 		return (1);
-	}
 	if (n % i == 0 && i > 0)
-	{
 		return (0);
-	}
 	return (acual_prime(n, i - 1));
 }
