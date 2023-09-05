3c6a09c8b128ca8bfc6b7de344dcb6f4827c7a97
ikram <izmerikram03@gmail.com>
2023-09-05 03:21:43 +0100

    function that creates a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 #include <unistd.h>
-#include <fnctl.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
