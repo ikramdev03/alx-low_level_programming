3c3837a08bba09fca09b9e3abbf519cdacf6a722
ikram <izmerikram03@gmail.com>
2023-09-06 22:20:05 +0100

    function that reads a text file and prints

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,25 +1,31 @@
 #include "main.h"
 
 /**
- * read_textfile- Reads text from a file and prints it.
- * @filename: name to file to read
- * @letters: number of letters to be read
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: number of bytes read and printed
+ * Return: If the function fails - -1.
+ * therwise - 1.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
-	free(buf);
+	int fd, w, len = 0;
+	
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)	
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+	
+	if (fd == -1 || w == -1)
+		return (-1);
+	
 	close(fd);
-	return (w);
+	return (1);
 }
