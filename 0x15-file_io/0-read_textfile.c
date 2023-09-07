2a9fb43e6e14b20ba53a54e8b50855527e4d9bd9
ikram <izmerikram03@gmail.com>
2023-09-07 01:36:00 +0100

    function that reads a text file and prints

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,31 +2,36 @@
 #include <stdlib.h>
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * read_textfile - Reads a text file and prints it to POSIX STDOUT.
+ * @filename: Apointer to the name of the file.
+ * @letters: The number of letters the 
+ *           function should read and print.
  *
- * Return: If the function fails - -1.
- * therwise - 1.
+ * Return: If the function fails or filename is NULL - 0
+ *         O/w - the acual number of bytes the function can read and print.
  */
-int create_file(const char *filename, char *text_content)
+ssize_t read_textfile(const char *filename, size_t letters);
 {
-	int fd, w, len = 0;
-	
+	ssize_t o, r, w;
+	char *buffer;
+
 	if (filename == NULL)
-		return (-1);
-	if (text_content != NULL)	
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w !=r)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		free(buffer);
+		return (0);
 	}
-	
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
-	
-	if (fd == -1 || w == -1)
-		return (-1);
-	
-	close(fd);
-	return (1);
+
+	free(buffer);
+	close(o);
+	return (w);
 }
